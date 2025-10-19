7770233dd653cd299191f091230e772cd2bceae1
valued mammal <valuedmammal@protonmail.com>
2025-10-18 22:22:29 -0500

    client: Add methods
    - `get_block_header_verbose`
    - `get_block_filter`
    - `get_block`

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,14 @@
 //! [`Client`].
 
-use bitcoin::BlockHash;
+use bitcoin::{Block, BlockHash};
 
 use corepc_client::bitcoin;
 use corepc_client::client_sync::Error;
+use corepc_client::types::model;
+use corepc_client::types::v29::{GetBlockFilter, GetBlockHeaderVerbose, GetBlockVerboseZero};
 use jsonrpc::Transport;
 use jsonrpc::{serde, serde_json};
+use serde_json::json;
 
 // RPC Client.
 #[derive(Debug)]
@@ -78,5 +81,26 @@ impl Client {
     pub fn get_best_block_hash(&self) -> Result<BlockHash, Error> {
         let res: String = self.call("getbestblockhash", &[])?;
         Ok(res.parse()?)
+    }
+
+    /// Get block header (verbose).
+    pub fn get_block_header_verbose(
+        &self,
+        hash: &BlockHash,
+    ) -> Result<model::GetBlockHeaderVerbose, Error> {
+        let res: GetBlockHeaderVerbose = self.call("getblockheader", &[json!(hash)])?;
+        Ok(res.into_model().unwrap())
+    }
+
+    /// Get block filter.
+    pub fn get_block_filter(&self, hash: &BlockHash) -> Result<model::GetBlockFilter, Error> {
+        let res: GetBlockFilter = self.call("getblockfilter", &[json!(hash)])?;
+        Ok(res.into_model().unwrap())
+    }
+
+    /// Get block.
+    pub fn get_block(&self, hash: &BlockHash) -> Result<Block, Error> {
+        let res: GetBlockVerboseZero = self.call("getblock", &[json!(hash), json!(0)])?;
+        Ok(res.block().unwrap())
     }
 }
