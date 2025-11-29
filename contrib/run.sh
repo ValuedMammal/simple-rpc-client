b984a243c71b0451ec7131588e534f499e0c32d3
valued mammal <valuedmammal@protonmail.com>
2025-11-29 17:43:26 -0500

    contrib: Update `run.sh`

--- contrib/run.sh
+++ contrib/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x
+# set -x
 set -euo pipefail
 
 # Throw an error message and exit.
@@ -18,16 +18,14 @@ fi
 path=$1
 
 # Terminate existing processes.
-killall bitcoind > /dev/null || true
+killall bitcoind > /dev/null 2>&1 || true
 
 # Set environment variables used for integration testing.
 export RPC_URL="127.0.0.1:18443"
 export RPC_COOKIE="${path}/regtest/.cookie"
 
 # Clear data directory.
-cd "$path"
-rm -rf regtest
-cd -
+rm -rf "${path}/regtest"
 
 # Start bitcoind in Regtest.
 bitcoind -regtest
@@ -35,14 +33,17 @@ bitcoind -regtest
 sleep 1
 
 # Create wallet.
-bitcoin-cli -regtest createwallet test
+echo 'Creating test wallet'
+bitcoin-cli -regtest createwallet test > /dev/null
 
 # Mine blocks.
+echo 'Generating blocks...'
 bitcoin-cli -regtest -generate 101 > /dev/null
 
 # Run integration tests.
-cargo test --test test_rpc_client -- --test-threads=1 --show-output
+cargo test --test test_rpc_client -- --test-threads=1
+# cargo test --test test_rpc_client -- --test-threads=1 --show-output
+# cargo test --test test_rpc_client -- test_get_blockchain_info --test-threads=1
 
 # Stop daemon.
 bitcoin-cli -regtest stop
-
